711bd9e9ba005472905a43a798cf7fde5f187c1b
Brodator <brodatorl@example.com>
2020-12-02 20:18:51 +0600

    Реализована телепортация героя

---
+++ Assets/Scripts/Jerk.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+public class Jerk : MonoBehaviour
+{
+    public Transform target;
+    public float distance = 10.0f;
+    public LayerMask mask;
+ 
+    private void Update()
+    {
+        if (Input.GetButton("Fire2"))
+        {
+            Time.timeScale = 0.2f;
+            var mousePos3 = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            //var mousePos = new Vector2(mousePos3.x, mousePos3.y);
+
+            //Physics2D.queriesStartInColliders = false;
+            //RaycastHit2D hitMouse = Physics2D.Raycast(transform.position, mousePos, distance, mask);
+            //target.gameObject.SetActive(true);
+
+
+            //if (hitMouse.collider == null)
+            //{
+            //    RaycastHit2D hit = Physics2D.Raycast(mousePos, Vector2.down, distance, mask);
+            //    target.position = hit.point;
+            //}
+            //else
+            //{
+            //    target.position = hitMouse.point;
+
+            Ray2D rayMouse = new Ray2D(transform.position, new Vector2(mousePos3.x, mousePos3.y));
+            Debug.DrawRay(rayMouse.origin, rayMouse.direction * distance, Color.black);
+            Physics2D.queriesStartInColliders = false;
+            RaycastHit2D hitMouse = Physics2D.Raycast(rayMouse.origin, rayMouse.direction, distance, mask);
+            target.gameObject.SetActive(true);
+
+            if (Physics2D.Raycast(rayMouse.origin, rayMouse.direction, distance, mask))
+            {
+                target.position = hitMouse.point;
+            }
+        }
+        if (Input.GetButtonUp("Fire2"))
+        {
+            transform.position = new Vector2(target.position.x, target.position.y + 1.0f);
+            target.gameObject.SetActive(false);
+            Time.timeScale = 1.0f;
+        }
+    }
+
+
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,71 @@
+﻿using DragonBones;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public float speed;
+    public float jumpForce;
+    private int countJump;
+    private UnityArmatureComponent armature;
+    public float animSpeed;
+    private float velocity;
+    public float jumpUp;
+    public float speedJump;
+    
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        armature = GetComponent<UnityArmatureComponent>();
+        //Cursor.visible = false;
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonUp("Horizontal") && !Input.GetButton("Horizontal"))
+        {
+            armature.animation.FadeIn("Idle", animSpeed);
+        }
+        if (Input.GetButtonDown("Horizontal"))
+        {
+            armature.animation.FadeIn("run", animSpeed);
+        }
+
+        Jump();
+    }
+
+    private void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && countJump < 2)
+        {
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            countJump++;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        velocity = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(velocity * speed, rb.velocity.y);
+
+
+        if (velocity < 0)
+        {
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+        }
+        if (velocity > 0)
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Floor"))
+        {
+            countJump = 0;
+        }
+    }
+}
